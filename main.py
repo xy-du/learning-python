c6ebfeac8571d82a13756a45b30450ebaad6aa70
xyDu <xy_du@qq.com>
2020-12-14 19:56:08 +0800

    some basic operation about string

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-# high-order functions: a function that takes a a function as a argument or returns a function as the result.
+firstname = 'austin'
+lastname = 'duke'
+print(f"{firstname} {lastname}")
 
-def reverse(word):
-    return word[::-1]
+fullname = f"{firstname} {lastname}"
+print(f"hello, {fullname.title()}. Welcome!!!")
 
+# the f-string is only begin with python 3.6, so for early version, you have to use format(), like bellow
+# fullname = "{} {}".format(firstname, lastname)
+# print(fullname)
 
-print(reverse('abcde'))
+space_str = ' python '
+print('-', space_str.rstrip(), '-')
+print('-', space_str.lstrip(), '-')
+print('-', space_str.strip(), '-')
+print('-', space_str, '-')  # only return changed values, no change happens to the original one.
+space_str = space_str.strip()
+print('-', space_str, '-')
 
-fruits = ['strawberry', 'fig', 'apple', 'cherry', 'raspberry', 'banana']
-# words in fruits list are not changed, only the reversed spelling is used
-s_list = sorted(fruits, key=reverse)
-print(s_list)
+# \t a tab, \n a newline
+print('\tpython')
+print('\npython')
+print('\n\tpython')
+
+# attention to syntax error, if you want use apostrophe in a string, make sure use double quote to quote it
+print("this is dick's house")
+
+# use % to do the string formation <format_string> % <values>
+# if there are multiple values, they have to be enclosed in a tuple
+line = '%d %s cost $%.2f' % (6, 'bananas', 1.74)
+print(line)
+# if there is only one value, it doesn't have to be enclosed
+line = '%d is a number' % 10
+print(line)
