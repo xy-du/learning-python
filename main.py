7205fc480fa56b02931eec1ed0bd21a76bb0c1ef
xyDu <xy_du@qq.com>
2020-12-14 10:41:32 +0800

    treat function like an object

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import test
+"""this is a docstring"""
 
-print('here is main.py. __name__ = ',__name__)
-if(__name__=='__main__'):
-    print('test.py is running directly')
-else:
-    print('test.py is running by others')
+def factorial(n):
+    """return n!"""
+    return 1 if n < 2 else factorial(n - 1) * n
+
+print(factorial(42))
+print(factorial.__doc__)
+print(type(factorial))
+
+fact=factorial
+print(fact(5))
+print(map(fact,range(0,11)))
+print(list(map(fact,range(0,11))))
+
+
+# import test
+#
+# print('here is main.py. __name__ = ',__name__)
+# if(__name__=='__main__'):
+#     print('test.py is running directly')
+# else:
+#     print('test.py is running by others')
