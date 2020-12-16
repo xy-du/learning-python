195e578ab45d716e0e7725a2b787065515a283a0
xyDu <xy_du@qq.com>
2020-12-16 13:52:34 +0800

    listcomps no longer leak their variables

--- ad_sequence.py
+++ ad_sequence.py
@@ -11,3 +11,26 @@ OR group by:
     immutable:
         tuple, str, bytes
 """
+
+"""
+list comprehensions(listcomps): quick way to build a sequence of list
+generator expression(genexps): a way to build sequences of other kinds
+"""
+
+symbols = '$¢£¥€¤'
+codes = []
+for s in symbols:
+    codes.append(ord(s))
+print(codes)
+
+# apparently this way is more concise
+# listcomps is meants to do one thing: build list
+# python ignore the line breaks in [] {} and (), so you can do multiple line listcomps
+codes = [ord(s) for s in symbols]
+print(codes)
+
+# in python2 the x used in listcomps will leak, i.e. it will affect the value surrounding it.
+# but this is fixed in python3, where variables in listcomps will just be a local var
+x = 'a'
+lx = [x for x in '1233']
+print(x)
