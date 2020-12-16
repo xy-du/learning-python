0735aa013af4c1f1900b215306a263cb91902967
xyDu <xy_du@qq.com>
2020-12-16 11:17:37 +0800

    overview of built-in sequeneces

---
+++ ad_sequence.py
@@ -0,0 +1,13 @@
+"""
+built-in sequences
+    container sequences:
+        list, tuple, cellections.deque.   they can hold difference type
+    flat sequences:
+        str, bytes, betearray, memoryview, array.array.   hold items of one type.
+
+OR group by:
+    mutable:
+        list, array.array, bytearray, collections.deque, memoryview
+    immutable:
+        tuple, str, bytes
+"""
