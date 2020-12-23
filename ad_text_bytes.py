39bd9f051c6462922f638746feb0e2e12f6c1e32
xyDu <xy_du@qq.com>
2020-12-23 14:38:44 +0800

    str/byte common method and construction of bytes

--- ad_text_bytes.py
+++ ad_text_bytes.py
@@ -16,3 +16,20 @@ ls = [1, 2, 3]
 print(ls[:1])
 str = 'this'
 print(str[:1])
+
+# bytes and bytearray support every str method except a few others that do format and depend on Unicode data
+# regular expresion in re module can also be supported is the regex is compiled from binary sequence instead of a str
+s_code = s_code.upper()
+print(s_code)
+print(s_code.isascii())
+print(s_code.decode('utf8'))
+
+# formhex is the method that only Binary sequence have and str don't
+# byte or bytearray can be built:
+# fromhex
+# constructor: str and encode method
+# constructor: iterable of which items are in range of 0~255
+# constructor: a single int, create a sequence of that size with null byte
+bf = bytes.fromhex('31 4B CE A9')
+print(bf)
+print(bf.decode())
