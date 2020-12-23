d38f1aabc12416dd31051b2e1d7c3ffe073039b5
xyDu <xy_du@qq.com>
2020-12-23 13:37:04 +0800

    slice only return one item (s[:1]) keep the origin type except str

--- ad_text_bytes.py
+++ ad_text_bytes.py
@@ -6,3 +6,13 @@ s_code = s.encode('utf-8')
 print(s_code)
 print(len(s_code))
 print(s_code.decode('utf-8'))
+
+# the only sequence type where s[0] == s[:1]  is str.
+# for every other sequence type ,s[i] returns one item, and s[i:i+1] returns a sequence of the same type with the s[1]
+# item inside it.
+tp = (1, 2, 4)
+print(tp[:1])
+ls = [1, 2, 3]
+print(ls[:1])
+str = 'this'
+print(str[:1])
