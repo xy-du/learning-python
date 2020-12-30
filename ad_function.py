dfbadc077b7d5841a79837a57232414b63effa5c
xyDu <xy_du@qq.com>
2020-12-30 11:26:57 +0800

    high-order functions

--- ad_function.py
+++ ad_function.py
@@ -10,12 +10,27 @@ def factorial(n):
     return 1 if n < 2 else n * factorial(n - 1)
 
 
-print(factorial(42))
-print(factorial.__doc__)
-print(type(factorial))
+# print(factorial(42))
+# print(factorial.__doc__)
+# print(type(factorial))
+#
+# fact = factorial
+# print(fact)
+# fact(5)
+# print(map(factorial, range(11)))
+# print(list(map(fact, range(11))))
 
-fact = factorial
-print(fact)
-fact(5)
-print(map(factorial, range(11)))
-print(list(map(fact, range(11))))
+
+# high-order functions:
+# receive function as args, return function as result
+# eg.  map, filter, reduce, apply
+fruits = ['strawberry', 'fig', 'apple', 'cherry', 'raspberry', 'banana']
+print(sorted(fruits, key=len))
+
+
+def reverse(words):
+    return words[::-1]
+
+
+# the origin data is not changed
+print(sorted(fruits, key=reverse))
