edd3e1ebd4fae69242b468e8440964ff28322cbc
xyDu <xy_du@qq.com>
2021-02-14 21:37:52 +0800

    call iter() to get a generator produced by yield

---
+++ iterator_generator/sentence_v3.py
@@ -0,0 +1,28 @@
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    # when iter() get invoked, a generator is built and returned,
+    # since the __iter__ here is a generator function
+    def __iter__(self):
+        for w in self.words:
+            yield w
+        return
+
+
+if __name__ == '__main__':
+    s = Sentence('"The time has come," the Walrus said,')
+    print(s)
+    for word in s:
+        print(word)
+    print(list(s))
