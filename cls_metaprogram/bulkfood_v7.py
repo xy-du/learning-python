4f07fc29db608f607ac99c1f2976fc3ac9f22a74
xyDu <xy_du@qq.com>
2021-02-28 16:13:54 +0800

    using metaclass to customize descriptor

---
+++ cls_metaprogram/bulkfood_v7.py
@@ -0,0 +1,20 @@
+import cls_metaprogram.model_v2 as model
+
+
+class LineItem(model.Entity):
+    description = model.NonBlank()
+    weight = model.Quantity()
+    price = model.Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+if __name__ == '__main__':
+    potato = LineItem('potato', 10, 9)
+    print(potato.__dict__)
